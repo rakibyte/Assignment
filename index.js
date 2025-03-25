65d9b5feee61b4e0c73024785fd5fcd83137fd70
rakibyte <rakibmarvel123@gmail.com>
2025-03-26 01:42:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV  I am rakib khan ')
+  res.send('Hello MCKV  I am RAKIB KHAN FROM CSE-DS ')
 })
 
 app.get('/courses', function (req, res) {
